b1941575f1f87e50fb9fc0beeebf1342a5b0f11f
Louiml <raz@louiml.net>
2024-03-21 22:50:39 +0200

    d

--- svelte.config.js
+++ svelte.config.js
@@ -4,18 +4,18 @@ const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter: adapter()
-		// adapter: firebase()
-		adapter: adapter(),
-		prerender: {
-			default: true
-		},
-		paths: {
-			base: dev ? '' : '/FearOfElronSite'
-		},
-		appDir: 'internal'
-	}
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : '/FearOfElronSite'
+        },
+        appDir: 'internal'
+    },
+    prerender: {
+        enabled: true,
+        onError: 'fail',
+        entries: ['*']
+    }
 };
 
 export default config;
