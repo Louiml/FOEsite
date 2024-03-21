dc5b8c0773276b549c77f52cd91224855fbac999
Louiml <raz@louiml.net>
2024-03-21 22:47:42 +0200

    j

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		// adapter: adapter()
+		// adapter: firebase()
+		adapter: adapter(),
+		prerender: {
+			default: true
+		},
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			base: dev ? '' : '/your-repo-name'
+		},
+		appDir: 'internal'
 	}
 };
 
